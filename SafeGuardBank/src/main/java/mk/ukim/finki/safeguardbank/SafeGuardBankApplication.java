66fffc03c06e95677e59ce3428121523793835f4
kbogojoska <107806557+kbogojoska@users.noreply.github.com>
2024-02-27 18:59:15 +0100

    Adding base web app

---
+++ SafeGuardBank/src/main/java/mk/ukim/finki/safeguardbank/SafeGuardBankApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.safeguardbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeGuardBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeGuardBankApplication.class, args);
+    }
+
+}
---
+++ SafeGuardBank/src/test/java/mk/ukim/finki/safeguardbank/SafeGuardBankApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.safeguardbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeGuardBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
